817be9bcb79e6549e75e857b72bbecdf4c87a0b5
AAK-sys <akmk2237@gmail.com>
2024-06-18 11:14:37 -0400

    permit all users to sign up and login

--- server/ResumeBuilder/src/main/java/learn/resume_builder/security/SecurityConfig.java
+++ server/ResumeBuilder/src/main/java/learn/resume_builder/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/login").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JwtRequestFilter(authenticationManagerBean(), converter))
