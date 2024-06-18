ddc68ff9db11a0e6b3e5c3c375ef83b7f5da888c
TommyLiang1 <tommyliang131@gmail.com>
2024-06-18 12:10:06 -0400

    Remove unnecessary files and add global css to index.css

--- client/resume-builder-client/src/App.js
+++ client/resume-builder-client/src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
-import {Routes, Route} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 
 function App() {
-  return (
-    <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/signup" element={<SignUp/>} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
