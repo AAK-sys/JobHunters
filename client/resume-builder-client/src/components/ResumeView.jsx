c4fd79c7afc6aef8a90fd644aa06260a25808616
TommyLiang1 <tommyliang131@gmail.com>
2024-06-19 22:10:30 -0400

    Add view all users table for admin privilege

--- client/resume-builder-client/src/components/ResumeView.jsx
+++ client/resume-builder-client/src/components/ResumeView.jsx
@@ -13,9 +13,9 @@ function ResumeView({
             backgroundColor: "#FFF",
         },
         section: {
-            padding: 10,
+            padding: 5,
             width: "85%",
-            margin: "10 auto",
+            margin: "0 auto",
         },
         title: {
             display: "block",
@@ -24,6 +24,10 @@ function ResumeView({
         },
         text: {
             fontSize: 14,
+        },
+        description: {
+            fontSize: 14,
+            marginTop: 10,
         },
         spread: {
             display: "flex",
@@ -43,13 +47,13 @@ function ResumeView({
                 flexDirection: "row",
                 justifyContent: "space-evenly",
                 fontSize: 14,
-                marginTop: 14,
+                marginTop: 10,
             },
         },
         summary: {
             view: {
                 display: "block",
-                marginTop: 20,
+                marginTop: 10,
             },
             displayName: {
                 display: "block",
@@ -57,7 +61,7 @@ function ResumeView({
                 textAlign: "center",
             },
             description: {
-                marginTop: 15,
+                marginTop: 10,
                 fontSize: 14,
             },
         },
@@ -65,11 +69,15 @@ function ResumeView({
             view: {
                 display: "flex",
                 flexDirection: "column",
-                marginTop: 15,
+                marginTop: 10,
                 gap: 5,
             },
         },
         skill: {
+            view: {
+                marginTop: 10,
+                marginBottom: 20,
+            },
             spread: {
                 display: "flex",
                 flexDirection: "row",
@@ -100,110 +108,113 @@ function ResumeView({
                     </View>
                 </View>
                 {/* Summary */}
-                <View style={styles.section}>
-                    {selectSummaries.length > 0 && (
+                {selectSummaries.length > 0 && (
+                    <View style={styles.section}>
                         <Text style={styles.title}>Summary</Text>
-                    )}
-                    {selectSummaries.map((summary) => {
-                        const val = summary.value;
-                        return (
-                            <View style={styles.summary.view}>
-                                <Text style={styles.summary.displayName}>
-                                    {val.displayName}
-                                </Text>
-                                <Text style={styles.summary.description}>
-                                    {val.description}
-                                </Text>
-                            </View>
-                        );
-                    })}
-                </View>
-                {/* Education */}
-                <View style={styles.section}>
-                    {selectEducations.length > 0 && (
-                        <Text style={styles.title}>Education</Text>
-                    )}
-                    {selectEducations.map((education) => {
-                        const val = education.value;
-                        return (
-                            <View style={styles.education.view}>
-                                <View style={styles.spread}>
-                                    <Text style={styles.text}>
-                                        {val.universityName}
+
+                        {selectSummaries.map((summary) => {
+                            const val = summary.value;
+                            return (
+                                <View style={styles.summary.view}>
+                                    <Text style={styles.summary.displayName}>
+                                        {val.displayName}
                                     </Text>
-                                    <Text style={styles.text}>
-                                        {val.startDate}
-                                        {val.endDate
-                                            ? ` - ${val.endDate}`
-                                            : " - Present"}
-                                    </Text>
-                                </View>
-                                <View style={styles.spread}>
-                                    <Text style={styles.text}>
-                                        {val.degree} in {val.major}
-                                    </Text>
-                                    {val.gpa && (
-                                        <Text style={styles.text}>
-                                            Cumulative GPA: {val.gpa}
-                                        </Text>
-                                    )}
-                                </View>
-                                {val.description && (
-                                    <View>
-                                        <Text style={styles.text}>
-                                            {val.description}
-                                        </Text>
-                                    </View>
-                                )}
-                            </View>
-                        );
-                    })}
-                </View>
-                {/* Experience */}
-                <View style={styles.section}>
-                    {selectExperiences.length > 0 && (
-                        <Text style={styles.title}>Experience</Text>
-                    )}
-                    {selectExperiences.map((experience) => {
-                        const val = experience.value;
-                        return (
-                            <View style={styles.education.view}>
-                                <View style={styles.spread}>
-                                    <Text style={styles.text}>
-                                        {val.companyName} | {val.role}
-                                    </Text>
-                                    <Text style={styles.text}>
-                                        {val.startDate}
-                                        {val.endDate
-                                            ? ` - ${val.endDate}`
-                                            : " - Present"}
-                                    </Text>
-                                </View>
-                                {val.description && (
-                                    <Text style={styles.text}>
+                                    <Text style={styles.summary.description}>
                                         {val.description}
                                     </Text>
-                                )}
-                            </View>
-                        );
-                    })}
-                </View>
-                {/* Skills */}
-                <View>
-                    {selectSkills.length > 0 && (
-                        <Text style={styles.title}>Skill</Text>
-                    )}
-                    <View style={styles.skill.spread}>
-                        {selectSkills.map((skill) => {
-                            const val = skill.value;
-                            return (
-                                <Text style={styles.skill.text}>
-                                    {val.name}
-                                </Text>
+                                </View>
                             );
                         })}
                     </View>
-                </View>
+                )}
+                {/* Education */}
+                {selectEducations.length > 0 && (
+                    <View style={styles.section}>
+                        <Text style={styles.title}>Education</Text>
+                        {selectEducations.map((education) => {
+                            const val = education.value;
+                            return (
+                                <View style={styles.education.view}>
+                                    <View style={styles.spread}>
+                                        <Text style={styles.text}>
+                                            {val.universityName}
+                                        </Text>
+                                        <Text style={styles.text}>
+                                            {val.startDate}
+                                            {val.endDate
+                                                ? ` - ${val.endDate}`
+                                                : " - Present"}
+                                        </Text>
+                                    </View>
+                                    <View style={styles.spread}>
+                                        <Text style={styles.text}>
+                                            {val.degree} in {val.major}
+                                        </Text>
+                                        {val.gpa && (
+                                            <Text style={styles.text}>
+                                                Cumulative GPA: {val.gpa}
+                                            </Text>
+                                        )}
+                                    </View>
+                                    {val.description && (
+                                        <View>
+                                            <Text style={styles.description}>
+                                                {val.description}
+                                            </Text>
+                                        </View>
+                                    )}
+                                </View>
+                            );
+                        })}
+                    </View>
+                )}
+
+                {/* Experience */}
+                {selectExperiences.length > 0 && (
+                    <View style={styles.section}>
+                        <Text style={styles.title}>Experience</Text>
+                        {selectExperiences.map((experience) => {
+                            const val = experience.value;
+                            return (
+                                <View style={styles.education.view}>
+                                    <View style={styles.spread}>
+                                        <Text style={styles.text}>
+                                            {val.companyName} | {val.role}
+                                        </Text>
+                                        <Text style={styles.text}>
+                                            {val.startDate}
+                                            {val.endDate
+                                                ? ` - ${val.endDate}`
+                                                : " - Present"}
+                                        </Text>
+                                    </View>
+                                    {val.description && (
+                                        <Text style={styles.description}>
+                                            {val.description}
+                                        </Text>
+                                    )}
+                                </View>
+                            );
+                        })}
+                    </View>
+                )}
+
+                {/* Skills */}
+                {selectSkills.length > 0 && (
+                    <View style={styles.skill.view}>
+                        <Text style={styles.title}>Skill</Text>
+                        <View style={styles.skill.spread}>
+                            {selectSkills.map((skill) => {
+                                const val = skill.value;
+                                return (
+                                    <Text style={styles.skill.text}>
+                                        {val.name}
+                                    </Text>
+                                );
+                            })}
+                        </View>
+                    </View>
+                )}
             </Page>
         </Document>
     );
